a4196ca6a6ec113dc7fed587ea1bd0fe4c65b746
teldommm <teldom14@gmail.com>
2026-09-02 19:39:57 +0300

    fix

--- app/src/main/feature/settings/contents/ComponentsScreen.kt
+++ app/src/main/feature/settings/contents/ComponentsScreen.kt
@@ -34,6 +34,7 @@ import androidx.compose.foundation.layout.heightIn
 import androidx.compose.foundation.layout.navigationBars
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.requiredHeightIn
+import androidx.compose.foundation.layout.safeDrawing
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.widthIn
